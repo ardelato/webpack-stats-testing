67a16b0aeeddbf3fe031c3a6658e7b958e880636
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-04-15 10:12:51 -0700

    add caching and use relative-ci commentor

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import _ from 'lodash';
+import { DateTime } from 'luxon';
 import './style.css';
 
 function component()
 {
   const element = document.createElement( 'div' );
 
+  const date = DateTime.now().toString();
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join( [ 'Hello', 'webpack' ], ' ' );
+  element.innerHTML = _.join( [ 'Hello', 'webpack at ', date ], ' ' );
   element.classList.add( 'hello' );
+
+
 
   return element;
 }
